b3542d267636431e5ab2d9b2889b54d087cc38ba
ChahirSaid <chahirsaid18@gmail.com>
2023-05-23 15:28:06 +0100

    main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * main - Main function of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * @envp: Array of environment variables
+ * Return: Integer
+ */
+int main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	signal(SIGINT, handle_ctrl);
+
+	prompt(argv, envp);
+
+	return (0);
+}
