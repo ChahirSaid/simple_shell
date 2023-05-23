61cb6db34546184359cb0cc2dc8a6ce7007d52bc
glackyy <glackycode@example.com>
2023-05-23 07:26:43 -0700

    _putchar

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success, the number of characters written is returned.
+ *         On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return write(STDOUT_FILENO, &c, 1);
+}
